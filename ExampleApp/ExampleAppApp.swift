74e5bc31ca5968c3bf340de12a3ff8d7a8888f27
Shawn Koh <shawn@shawnkoh.sg>
2021-05-25 00:23:08 +0800

    Add AppViewModel

--- ExampleApp/ExampleAppApp.swift
+++ ExampleApp/ExampleAppApp.swift
@@ -6,9 +6,20 @@
 //
 
 import SwiftUI
+import Resolver
+
+class AppViewModel: ObservableObject {
+    @Injected var firebaseService: FirebaseService
+
+    init() {
+        firebaseService.configure()
+    }
+}
 
 @main
 struct ExampleAppApp: App {
+    @StateObject var viewModel = AppViewModel()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
