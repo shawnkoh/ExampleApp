058bc0ce41ed4207762f59dd2c3f1f6f0e49a024
Shawn Koh <shawn@shawnkoh.sg>
2021-05-25 00:14:44 +0800

    Initial Commit

---
+++ ExampleApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ExampleApp
+//
+//  Created by Shawn Koh on 25/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ExampleApp/ExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by Shawn Koh on 25/5/21.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
